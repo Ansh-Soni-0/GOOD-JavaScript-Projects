ce54d32cd792fa0b5cc931d92ed94549cd480c12
ansh soni <anshsoni200002@gmail.com>
2025-03-31 00:50:44 +0530

    country search menu

---
+++ custom-menu-bar/script.js
@@ -0,0 +1,259 @@
+const wrapper = document.querySelector(".wrapper");
+const selectBtn = wrapper.querySelector(".select-btn");
+const countryContainer = wrapper.querySelector(".options");
+const inputField = wrapper.querySelector("input");
+
+const countries = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cabo Verde",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Comoros",
+  "Congo",
+  "Costa Rica",
+  "Croatia",
+  "Cuba",
+  "Cyprus",
+  "Czechia",
+  "Democratic Republic of the Congo",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Eswatini",
+  "Ethiopia",
+  "Fiji",
+  "Finland",
+  "France",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Morocco",
+  "Mozambique",
+  "Myanmar (Burma)",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "North Korea",
+  "North Macedonia",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Palestine",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Qatar",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Vincent and the Grenadines",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "South Korea",
+  "South Sudan",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor-Leste",
+  "Togo",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Vatican City",
+  "Venezuela",
+  "Vietnam",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
+
+const addCountry = (selectedCountry) => {
+    countryContainer.innerHTML = "";
+    countries.map(country => {
+
+        let isSelected = country == selectedCountry ? "selected" : "";
+
+        let li = `<li onClick="updateName(this)" class="${isSelected}">${country}</li>`;
+        countryContainer.insertAdjacentHTML("beforeend" , li);
+    });
+}
+
+addCountry();
+
+const updateName = (selectedLi) => {
+
+    inputField.value = "";
+
+    addCountry(selectedLi.innerHTML);
+    // console.log(selectedLi.innerHTML);
+    
+
+    //in select button have two child therefore we excess only first child from (firstElementChild)
+    selectBtn.firstElementChild.innerHTML = selectedLi.innerHTML
+    wrapper.classList.remove("active");
+
+    
+
+    addCountry(selectedLi.innerHTML);
+}
+
+inputField.addEventListener("keyup" , () => {
+    // console.log(inputField.value)
+    let arr = [];
+    const searchedValue = inputField.value;
+    // console.log(searchedValue);
+
+    arr = countries.filter(data => {
+        return data.toLocaleLowerCase().startsWith(searchedValue) || data.startsWith(searchedValue) 
+ 
+    })
+    
+    arr = arr.map((filterCountry) => `<li onClick="updateName(this)">${filterCountry}</li>`).join("")
+
+    
+
+    countryContainer.innerHTML = arr ? arr : `<p class="msg">Oops country not found! 😟</p>`;
+
+    
+})
+
+
+
+
+
+selectBtn.addEventListener("click", () => {
+  wrapper.classList.toggle("active");
+});
